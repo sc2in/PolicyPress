540a9c83405a0688e940f5c5120536bca4c6be20
TsunamiNoAi <spam@falseblue.com>
2025-06-10 17:17:39 -0400

    Refactor organization handling and improve file reading in pandoc.zig

--- src/pandoc.zig
+++ src/pandoc.zig
@@ -6,6 +6,7 @@ const math = std.math;
 const tomlz = @import("tomlz");
 
 var alloc: Allocator = undefined;
+var org: []const u8 = undefined;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -29,19 +30,25 @@ pub fn main() !void {
     defer config.deinit(alloc);
 
     const extra = config.getTable("extra") orelse return error.NoExtraInConfig;
+    org = extra.getString("organization") orelse return error.NoOrganizationInExtra;
     const policy_root = extra.getString("policy_root") orelse return error.NoPolicyRootInExtra;
-    std.debug.print("{s}\n", .{policy_root});
     // const redact = b.option(bool, "redact", "Redact PDFs") orelse false;
     // const draft = b.option(bool, "redact", "Redact PDFs") orelse false;
 
     const md_files = try find_md_files(workDir, policy_root);
+    defer {
+        for (md_files) |f|
+            f.close();
+        alloc.free(md_files);
+    }
     for (md_files) |file| {
-        std.debug.print("{s}\n", .{file});
+        std.debug.print("{}b\n", .{(try file.stat()).size});
     }
 }
 
-pub fn find_md_files(root: std.fs.Dir, policy_dir: []const u8) ![][]u8 {
-    var files = Array([]u8).init(alloc);
+pub fn find_md_files(root: std.fs.Dir, policy_dir: []const u8) ![]std.fs.File {
+    std.debug.print("Reading in policies from: {s}\n", .{policy_dir});
+    var files = Array(std.fs.File).init(alloc);
     defer files.deinit();
     var policy_root = try (try root.openDir("content", .{
         .access_sub_paths = true,
@@ -55,14 +62,14 @@ pub fn find_md_files(root: std.fs.Dir, policy_dir: []const u8) ![][]u8 {
     try find_inner(&files, policy_root);
     return try files.toOwnedSlice();
 }
-fn find_inner(files: *Array([]u8), start: std.fs.Dir) !void {
+fn find_inner(files: *Array(std.fs.File), start: std.fs.Dir) !void {
     var iter = start.iterate();
     while (try iter.next()) |entry| {
         switch (entry.kind) {
             .file => {
                 if (!std.mem.startsWith(u8, entry.name, "_") and
                     std.mem.endsWith(u8, entry.name, ".md"))
-                    try files.append(try files.allocator.dupe(u8, entry.name));
+                    try files.append(try start.openFile(entry.name, .{ .mode = .read_only }));
             },
             .directory => {
                 var sub = try start.openDir(entry.name, .{ .access_sub_paths = true, .iterate = true });

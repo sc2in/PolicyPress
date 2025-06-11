fec98dcc0a3e27836e27245fc272c5695ba4e5ca
TsunamiNoAi <spam@falseblue.com>
2025-06-11 11:38:37 -0400

    Add documentation and get PDFs generating mostly correctly. Added TODOs to address

--- src/pandoc.zig
+++ src/pandoc.zig
@@ -1,3 +1,9 @@
+///! This program automates the process of converting Markdown policy documents into styled PDF files.
+///! It loads configuration from a TOML file, processes Markdown files (including YAML front matter and custom placeholders),
+///! applies organization branding, and invokes Pandoc with a set of dynamically constructed arguments to generate PDFs.
+///! The build is highly configurable, supporting custom logos, organization names, color extraction from images,
+///! and options for draft/redacted document states. The system is designed for batch processing of policy directories,
+///! with robust error handling and logging at multiple stages of the pipeline.
 const std = @import("std");
 const Array = std.ArrayList;
 const Allocator = std.mem.Allocator;
@@ -8,9 +14,23 @@ const Yaml = @import("yaml").Yaml;
 const ctime = @cImport(@cInclude("time.h"));
 const mvzr = @import("mvzr");
 var alloc: Allocator = undefined;
-var org: []const u8 = undefined;
-var logo_path: []const u8 = undefined;
 var global_args: Array([]const u8) = undefined;
+var global_config: struct {
+    org: []u8 = undefined,
+    logo_path: []u8 = undefined,
+    color: []u8 = undefined,
+    current_year: []u8 = undefined,
+    root: []const u8 = undefined,
+    is_draft: bool = false,
+    redact: bool = false,
+
+    pub fn deinit(self: @This()) void {
+        alloc.free(self.org);
+        alloc.free(self.logo_path);
+        alloc.free(self.color);
+    }
+} = .{};
+
 pub const std_options: std.Options = .{
     .log_level = .info,
     .log_scope_levels = &[_]std.log.ScopeLevel{
@@ -19,6 +39,7 @@ pub const std_options: std.Options = .{
     },
     .logFn = logFn,
 };
+/// Custom logging function that prints log messages depending on the log level and scope.
 pub fn logFn(
     comptime level: std.log.Level,
     comptime scope: @TypeOf(.EnumLiteral),
@@ -36,11 +57,12 @@ pub fn logFn(
 const panlog = std.log.scoped(.pandoc);
 
 pub fn main() !void {
-    panlog.info("Hello from my_scope\n", .{});
-    panlog.info("Hello from default scope\n", .{});
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
-    alloc = gpa.allocator();
+    var arena = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer arena.deinit();
+
+    alloc = arena.allocator();
 
     var dt_str_buf: [40]u8 = undefined;
     const t = ctime.time(null);
@@ -48,10 +70,10 @@ pub fn main() !void {
     const format = "%Y";
     const dt_str_len = ctime.strftime(&dt_str_buf, dt_str_buf.len, format, lt);
     const current_year = dt_str_buf[0..dt_str_len];
-    _ = current_year;
-
+    global_config.current_year = current_year;
+    global_config.root = std.posix.getenv("DEVBOX_PROJECT_ROOT") orelse return error.ProjectRootNotFoundInEnv;
     var workDir = try std.fs.openDirAbsolute(
-        std.posix.getenv("DEVBOX_PROJECT_ROOT") orelse return error.ProjectRootNotFoundInEnv,
+        global_config.root,
         .{
             .access_sub_paths = true,
             .iterate = true,
@@ -68,12 +90,11 @@ pub fn main() !void {
     defer config.deinit(alloc);
 
     const extra = config.getTable("extra") orelse return error.NoExtraInConfig;
-    org = extra.getString("organization") orelse return error.NoOrganizationInExtra;
-    logo_path = extra.getString("logo") orelse return error.NoLogoInExtra;
-    const color = try get_logo_color(logo_path);
-    defer alloc.free(color);
-
     const policy_root = extra.getString("policy_root") orelse return error.NoPolicyRootInExtra;
+    const logo = extra.getString("logo") orelse return error.NoLogoInExtra;
+    global_config.logo_path = try std.fmt.allocPrint(alloc, "static/{s}", .{logo});
+    global_config.org = try alloc.dupe(u8, extra.getString("organization") orelse return error.NoOrgInExtra);
+    defer global_config.deinit();
     // const redact = b.option(bool, "redact", "Redact PDFs") orelse false;
     // const draft = b.option(bool, "redact", "Redact PDFs") orelse false;
 
@@ -83,7 +104,9 @@ pub fn main() !void {
         //     alloc.free(a);
         global_args.deinit();
     }
+
     try create_global_args(&global_args);
+    //TODO: Add output directory
 
     const md_files = try find_md_files(workDir, policy_root);
     defer {
@@ -95,13 +118,57 @@ pub fn main() !void {
     std.debug.print("Building PDFs from {} markdown files in {s} .. \n", .{ total_files, policy_root });
     for (md_files) |file| {
         try process_md_file(file);
+        break; // only process one for dev
     }
 }
+
+///Populates the global_args array with command-line arguments for Pandoc, based on the current global configuration
 fn create_global_args(args: *Array([]const u8)) !void {
+    global_config.color = try get_logo_color(global_config.logo_path);
+
+    const data_dir = try std.fmt.allocPrint(alloc, "--data-dir={s}", .{global_config.root});
+    try args.append(data_dir);
+
+    const res_dir = try std.fmt.allocPrint(alloc, "--resource-path={s}", .{global_config.root});
+    try args.append(res_dir);
+
+    const footer = try std.fmt.allocPrint(alloc, "footer-left={s} \\textcopyright {s}", .{ global_config.org, global_config.current_year });
+    try args.appendSlice(&.{ "-V", footer });
+    try args.appendSlice(&.{ "-V", "footer-center=Confidental" });
+
+    const header = try std.fmt.allocPrint(alloc, "header-right=\\includegraphics[width=2cm,height=2cm]{{{s}}}", .{global_config.logo_path});
+    try args.appendSlice(&.{ "-V", header });
+
+    const logo = try std.fmt.allocPrint(alloc, "titlepage-logo={s}", .{global_config.logo_path});
+    try args.appendSlice(&.{ "-V", logo });
+
+    const inst = try std.fmt.allocPrint(alloc, "institution=\"{s}\"", .{global_config.org});
+    try args.appendSlice(&.{ "-V", inst });
+
+    const color = try std.fmt.allocPrint(alloc, "titlepage-rule-color={s}", .{global_config.color[1..7]});
+    try args.appendSlice(&.{ "-V", color });
+
     try args.appendSlice(&.{ "-V", "papersize=letter" });
     try args.appendSlice(&.{ "-V", "titlepage=true" });
-}
+    try args.appendSlice(&.{ "-V", "table-use-row-colors=true " });
+    try args.appendSlice(&.{ "-V", "logo-width=6cm" });
+    try args.appendSlice(&.{ "-V", "toc-depth=3" });
+    try args.appendSlice(&.{ "-V", "toc=true" });
+    try args.appendSlice(&.{ "-V", "toc-own-page=true" });
+    try args.appendSlice(&.{ "-V", "titlepage=true" });
+    try args.appendSlice(&.{ "--template", "eisvogel" });
+    try args.append("--webtex");
+    try args.append("--listings");
+    try args.append("--pdf-engine=xelatex");
 
+    if (global_config.is_draft) {
+        try args.appendSlice(&.{ "-V", "page-background=static/draft.png" });
+        try args.appendSlice(&.{ "-V", "page-background-opacity=0.8" });
+    }
+    //TODO
+    // try args.append("-F mermaid-filter");
+}
+/// Processes a single markdown file: loads contents, applies replacements, extracts metadata, writes a temporary file, and invokes Pandoc to generate the PDF.
 fn process_md_file(md: std.fs.File) !void {
     const raw = try md.readToEndAlloc(alloc, 100_000_000);
     var contents = Array(u8){
@@ -127,9 +194,15 @@ fn process_md_file(md: std.fs.File) !void {
 
     panlog.debug("{}\n", .{fm});
     try local.insert(0, "pandoc");
-    try local.appendSlice(&.{ "tmp.md", "-o", "tmp.pdf" });
+    const out_file = try fm.filename(alloc);
+    // defer alloc.free(out_file);
+
+    try local.appendSlice(&.{ "tmp.md", "-o", out_file });
+    //TODO: Add the local resource folder
     try run_pandoc(local);
 }
+
+/// Parses YAML front matter from a markdown file, extracts document metadata, formats the title, and returns a FrontMatter struct.
 fn get_metadata(txt: *Array(u8)) !FrontMatter {
     const end_fm = std.mem.indexOfPos(u8, txt.items, 3, "---") orelse return error.InvalidFrontMatter;
     var y: Yaml = .{ .source = txt.items[3..end_fm] };
@@ -156,8 +229,23 @@ fn get_metadata(txt: *Array(u8)) !FrontMatter {
 
     const most_recent = major_revisions[0];
     const m = try most_recent.asMap();
+
+    const rd = "{s} (Redacted) (Draft)";
+    const r = "{s} (Redacted)";
+    const d = "{s} (Draft)";
+    const e = "{s}";
+    const t = map.get("title").?.string;
+    const title = if (global_config.redact and global_config.is_draft)
+        try std.fmt.allocPrint(alloc, rd, .{t})
+    else if (global_config.redact)
+        try std.fmt.allocPrint(alloc, r, .{t})
+    else if (global_config.is_draft)
+        try std.fmt.allocPrint(alloc, d, .{t})
+    else
+        try std.fmt.allocPrint(alloc, e, .{t});
+
     return .{
-        .title = try alloc.dupe(u8, map.get("title").?.string),
+        .title = title,
         .last_reviewed = try alloc.dupe(u8, extra.get("last_reviewed").?.string),
         .most_recent_version = switch (m.get("version").?) {
             .string => |s| try alloc.dupe(u8, s),
@@ -170,7 +258,7 @@ const FrontMatter = struct {
     title: []u8,
     most_recent_version: []u8,
     last_reviewed: []u8,
-
+    /// Formats the front matter information and writes it to the provided writer.
     pub fn format(self: FrontMatter, comptime _: []const u8, _: anytype, writer: anytype) !void {
         try writer.print(
             "{s}\n\tVersion: {s}\tLast Reviewed: {s}\n",
@@ -181,6 +269,10 @@ const FrontMatter = struct {
             },
         );
     }
+    /// Generates a PDF filename from the title and most recent version in the front matter.
+    pub fn filename(self: FrontMatter, a: Allocator) ![]u8 {
+        return std.fmt.allocPrint(a, "{s} - v{s}.pdf", .{ self.title, self.most_recent_version });
+    }
 
     pub fn deinit(self: *FrontMatter) void {
         alloc.free(self.title);
@@ -189,6 +281,7 @@ const FrontMatter = struct {
     }
 };
 
+/// Comparator function for sorting YAML revision values in ascending order.
 fn revisions_lt(_: @TypeOf(.{}), a: Yaml.Value, b: Yaml.Value) bool {
     const as = a.string;
     const bs = b.string;
@@ -199,11 +292,23 @@ fn revisions_lt(_: @TypeOf(.{}), a: Yaml.Value, b: Yaml.Value) bool {
     return false;
 }
 
+/// Replaces all instances of the organization placeholder in the markdown text with the actual organization name from the global configuration.
 fn replace_org(txt: *Array(u8)) !void {
-    const size = std.mem.replacementSize(u8, txt.items, "{{ org() }}", org);
+    const size = std.mem.replacementSize(
+        u8,
+        txt.items,
+        "{{ org() }}",
+        global_config.org,
+    );
     const buf = try alloc.alloc(u8, size);
 
-    _ = std.mem.replace(u8, txt.items, "{{ org() }}", org, buf);
+    _ = std.mem.replace(
+        u8,
+        txt.items,
+        "{{ org() }}",
+        global_config.org,
+        buf,
+    );
     txt.deinit();
     txt.* = .{
         .allocator = alloc,
@@ -212,6 +317,7 @@ fn replace_org(txt: *Array(u8)) !void {
     };
 }
 
+/// Finds and replaces custom Mermaid code blocks in the markdown with a standardized code block format.
 fn replace_mermaid(txt: *Array(u8)) !void {
     const mermaid: mvzr.Regex = mvzr.compile("\\{%\\s*mermaid\\(\\)\\s*%\\}.+?\\{%\\s*end\\s*%\\}").?;
     // const mermaid_end: mvzr.Regex = mvzr.compile("\\{%\\s*end\\s*\\}").?;
@@ -248,8 +354,9 @@ fn replace_mermaid(txt: *Array(u8)) !void {
     }
 }
 
+/// Recursively finds and opens all markdown files in the specified policy directory, returning them as an array of files.
 pub fn find_md_files(root: std.fs.Dir, policy_dir: []const u8) ![]std.fs.File {
-    panlog.debug("Reading in policies from: {s}\n", .{policy_dir});
+    panlog.info("Reading in policies from: {s}\n", .{policy_dir});
     var files = Array(std.fs.File).init(alloc);
     defer files.deinit();
     var policy_root = try (try root.openDir("content", .{
@@ -264,6 +371,7 @@ pub fn find_md_files(root: std.fs.Dir, policy_dir: []const u8) ![]std.fs.File {
     try find_inner(&files, policy_root);
     return try files.toOwnedSlice();
 }
+/// Helper function to recursively traverse directories and append markdown files to the provided array.
 fn find_inner(files: *Array(std.fs.File), start: std.fs.Dir) !void {
     var iter = start.iterate();
     while (try iter.next()) |entry| {
@@ -283,13 +391,11 @@ fn find_inner(files: *Array(std.fs.File), start: std.fs.Dir) !void {
     }
 }
 
+/// Runs an external command to extract the dominant color from the logo image and returns it as a string.
 fn get_logo_color(path: []const u8) ![]u8 {
-    const full_path = try std.fmt.allocPrint(alloc, "static/{s}", .{path});
-    defer alloc.free(full_path);
-
     const argv = [_][]const u8{
         "magick",
-        full_path,
+        path,
         "-scale",
         "1x1\\!",
         "-format",
@@ -313,7 +419,12 @@ fn get_logo_color(path: []const u8) ![]u8 {
     panlog.debug("{any} {s}\n", .{ exit_code, err.items });
     return try out.toOwnedSlice(alloc);
 }
+
+/// Spawns a Pandoc process with the provided arguments, collects output, and logs errors or results as needed.
 fn run_pandoc(args: Array([]const u8)) !void {
+    panlog.debug("Running pandoc with args:\n", .{});
+    for (args.items) |a|
+        panlog.debug("\t{s}\n", .{a});
     var child = std.process.Child.init(args.items, alloc);
     child.stdout_behavior = .Pipe;
     child.stderr_behavior = .Pipe;
@@ -327,8 +438,10 @@ fn run_pandoc(args: Array([]const u8)) !void {
     try child.collectOutput(alloc, &out, &err, 100_000);
 
     const exit_code = child.wait() catch |e| {
-        panlog.debug("Error in pandoc: {s}\nRan with: {s}\n", .{ err.items, args.items });
+        panlog.err("Error in pandoc: {s}\nRan with: {s}\n", .{ err.items, args.items });
         return e;
     };
     panlog.debug("{any} {s}\n", .{ exit_code, out.items });
+    if (err.items.len > 0)
+        panlog.err("{s}\n", .{err.items});
 }

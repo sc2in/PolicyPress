91f6e049843d04182ece4a3b488f517ac8138e69
TsunamiNoAi <spam@falseblue.com>
2025-06-28 03:20:55 -0400

    fix output path formatting in PDF generation

--- build.zig
+++ build.zig
@@ -1,3 +1,5 @@
+//! Copyright © 2025 [Star City Security Consulting, LLC (SC2)](https://sc2.in)
+//! SPDX-License-Identifier: AGPL-3.0-or-later
 const std = @import("std");
 const Array = std.ArrayList;
 const Allocator = std.mem.Allocator;
@@ -214,8 +216,8 @@ pub fn build(b: *std.Build) !void {
 
                 .install_subdir = "pdfs",
             });
-            pdf_step.dependOn(&inst.step);
             inst.step.dependOn(&run_wrapper.step);
+            pdf_step.dependOn(&inst.step);
             // Step 3: Install the generated PDF
             _ = wf.addCopyDirectory(
                 pdf_dir.path(b, ""),
